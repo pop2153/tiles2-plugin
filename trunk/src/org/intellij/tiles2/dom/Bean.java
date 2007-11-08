afb604ad5e0d3c39028b069786679ea68797f0e3
libing.chen <libing.chen@43b8a67c-e93d-0410-8a6d-b104505a36b8>
2007-11-08 03:42:24 +0000

    bean element

---
+++ trunk/src/org/intellij/tiles2/dom/Bean.java
@@ -0,0 +1,21 @@
+package org.intellij.tiles2.dom;
+
+import com.intellij.javaee.model.xml.CommonDomModelElement;
+import com.intellij.psi.PsiClass;
+import com.intellij.util.xml.*;
+
+import java.util.List;
+
+/**
+ * bean element.
+ *
+ * @author jacky
+ */
+public interface Bean extends CommonDomModelElement {
+
+    @Attribute("classtype")
+    public GenericAttributeValue<PsiClass> geClasstype();
+
+    @SubTagList("set-property")
+    public List<SetProperty> getSetPropertyList();
+}
