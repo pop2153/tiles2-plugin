e6c413a5891bf3f7e7a229d85b5ed9458194834c
libing.chen <libing.chen@43b8a67c-e93d-0410-8a6d-b104505a36b8>
2007-11-08 06:36:55 +0000

    setProperty implementation

---
+++ src/org/intellij/tiles2/dom/impl/SetPropertyImpl.java
@@ -0,0 +1,12 @@
+package org.intellij.tiles2.dom.impl;
+
+import com.intellij.javaee.model.xml.impl.BaseImpl;
+import org.intellij.tiles2.dom.SetProperty;
+
+/**
+ * setProperty implementation
+ *
+ * @author jacky
+ */
+public abstract class SetPropertyImpl extends BaseImpl implements SetProperty {
+}
