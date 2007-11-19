6667990020e0e36ec2c25700dc0afe10ec826766
libing.chen <libing.chen@43b8a67c-e93d-0410-8a6d-b104505a36b8>
2007-11-19 06:46:12 +0000

    minor update

--- trunk/src/org/intellij/tiles2/Tiles2Constants.java
+++ trunk/src/org/intellij/tiles2/Tiles2Constants.java
@@ -11,9 +11,11 @@ import javax.swing.*;
  */
 public interface Tiles2Constants {
     public String TILES2_CONFIG_DTD = "http://tiles.apache.org/dtds/tiles-config_2_0.dtd";
+    public String STRUTS_CONFIG_DTD = "http://struts.apache.org/dtds/struts-2.0.dtd";
     public String TILES2_TAGLIB_SCHEMA = "http://tiles.apache.org/tags-tiles";
     public Icon TILES2_LOGO = IconLoader.findIcon("/icons/tiles2_logo.png");
     public Icon TILES2_CONFIG_LOGO = IconLoader.findIcon("/icons/TilesConfig.png");
+    public Icon TILES2_DEFINITION_LOGO = IconLoader.findIcon("/icons/definition.png");
 
     public String DEFINITIONS_CONFIG = "org.apache.tiles.impl.BasicTilesContainer.DEFINITIONS_CONFIG";
     public String TILES_SERVLET = "org.apache.tiles.web.startup.TilesServlet";
