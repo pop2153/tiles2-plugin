2032d2288fe3ca6d03aebdcb6c5a7e5e681478c9
libing.chen <libing.chen@43b8a67c-e93d-0410-8a6d-b104505a36b8>
2008-12-09 02:38:37 +0000

    minor update

--- src/org/intellij/tiles2/impl/Tiles2ConfigModelImpl.java
+++ src/org/intellij/tiles2/impl/Tiles2ConfigModelImpl.java
@@ -1,7 +1,7 @@
 package org.intellij.tiles2.impl;
 
 import com.intellij.psi.xml.XmlFile;
-import com.intellij.util.xml.model.DomModelImpl;
+import com.intellij.util.xml.model.impl.DomModelImpl;
 import org.intellij.tiles2.Tiles2ConfigModel;
 import org.intellij.tiles2.dom.TilesDefinitions;
 import org.jetbrains.annotations.NotNull;
