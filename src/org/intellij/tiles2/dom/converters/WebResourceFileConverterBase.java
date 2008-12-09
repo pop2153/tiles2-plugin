92088b75e8f909b0d0b70872c604d4b10d46bf0d
libing.chen <libing.chen@43b8a67c-e93d-0410-8a6d-b104505a36b8>
2008-12-09 08:16:06 +0000

    minor update

--- src/org/intellij/tiles2/dom/converters/WebResourceFileConverterBase.java
+++ src/org/intellij/tiles2/dom/converters/WebResourceFileConverterBase.java
@@ -1,23 +1,23 @@
 package org.intellij.tiles2.dom.converters;
 
-import com.intellij.javaee.web.facet.WebFacet;
 import com.intellij.javaee.web.WebRoot;
+import com.intellij.javaee.web.facet.WebFacet;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.ModuleRootManager;
-import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
-import com.intellij.psi.impl.source.resolve.reference.ProcessorRegistry;
-import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
-import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSetBase;
-import com.intellij.psi.infos.CandidateInfo;
-import com.intellij.psi.scope.PsiConflictResolver;
-import com.intellij.psi.scope.PsiScopeProcessor;
+import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSet;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.OrderedSet;
-import com.intellij.util.xml.*;
-import org.jetbrains.annotations.*;
+import com.intellij.util.xml.ConvertContext;
+import com.intellij.util.xml.CustomReferenceConverter;
+import com.intellij.util.xml.GenericDomValue;
+import com.intellij.util.xml.ResolvingConverter;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
@@ -28,7 +28,8 @@ import java.util.*;
  */
 public abstract class WebResourceFileConverterBase extends ResolvingConverter<PsiFile> implements CustomReferenceConverter {
 
-    @Nullable public PsiFile fromString(@Nullable @NonNls String s, final ConvertContext context) {
+    @Nullable
+    public PsiFile fromString(@Nullable @NonNls String s, final ConvertContext context) {
         if (s == null) return null;
         final PsiReference[] references = createReferences((GenericDomValue) context.getInvocationElement(), context.getReferenceXmlElement(), context);
         if (references.length == 0) return null;
@@ -36,7 +37,8 @@ public abstract class WebResourceFileConverterBase extends ResolvingConverter<Ps
         return element instanceof PsiFile ? (PsiFile) element : null;
     }
 
-    @Nullable public String toString(@Nullable PsiFile psiFile, final ConvertContext context) {
+    @Nullable
+    public String toString(@Nullable PsiFile psiFile, final ConvertContext context) {
         if (psiFile == null) return null;
         final VirtualFile file = psiFile.getVirtualFile();
         if (file == null) return null;
@@ -67,7 +69,8 @@ public abstract class WebResourceFileConverterBase extends ResolvingConverter<Ps
         return Collections.emptyList();
     }
 
-    @Nullable public PsiElement resolve(final PsiFile o, final ConvertContext context) {
+    @Nullable
+    public PsiElement resolve(final PsiFile o, final ConvertContext context) {
         return isFileAccepted(o) ? super.resolve(o, context) : null;
     }
 
@@ -77,16 +80,16 @@ public abstract class WebResourceFileConverterBase extends ResolvingConverter<Ps
         if (s == null) {
             return PsiReference.EMPTY_ARRAY;
         }
-        final int offset = ReferenceProvidersRegistry.getInstance(element.getProject()).getOffsetInElement(element);
-        final FileReferenceSetBase set = createReferenceSet(context, s, element, offset);
+        final int offset = ElementManipulators.getOffsetInElement(element);
+        final FileReferenceSet set = createReferenceSet(context, s, element, offset);
         return set.getAllReferences();
     }
 
     protected abstract boolean isFileAccepted(final PsiFile file);
 
-    protected FileReferenceSetBase createReferenceSet(final ConvertContext context, final String text, final PsiElement element,
-                                                      final int startInElement) {
-        return new FileReferenceSetBase(text, element, startInElement, null, true) {
+    protected FileReferenceSet createReferenceSet(final ConvertContext context, final String text, final PsiElement element,
+                                                  final int startInElement) {
+        return new FileReferenceSet(text, element, startInElement, null, true) {
             protected boolean isSoft() {
                 return false;
             }
@@ -97,23 +100,16 @@ public abstract class WebResourceFileConverterBase extends ResolvingConverter<Ps
                 return addDefaultRoots(result, context);
             }
 
-            protected PsiScopeProcessor createProcessor(final List<CandidateInfo> candidateInfos, List<Class> classes, List<PsiConflictResolver> psiConflictResolvers) throws ProcessorRegistry.IncompatibleReferenceTypeException {
-                final PsiScopeProcessor baseProcessor = super.createProcessor(candidateInfos, classes, psiConflictResolvers);
-                return new PsiScopeProcessor() {
-                    public boolean execute(PsiElement element, PsiSubstitutor substitutor) {
+            @Override
+            protected Condition<PsiElement> createCondition() {
+                return new Condition<PsiElement>() {
+                    public boolean value(PsiElement psiElement) {
                         final boolean isFile = element instanceof PsiFile;
-                        return isFile && !isFileAccepted((PsiFile) element) || baseProcessor.execute(element, substitutor);
-                    }
-
-                    public <T> T getHint(Class<T> hintClass) {
-                        return baseProcessor.getHint(hintClass);
-                    }
-
-                    public void handleEvent(Event event, Object associated) {
-                        baseProcessor.handleEvent(event, associated);
+                        return isFile && !isFileAccepted((PsiFile) element);
                     }
                 };
             }
+
         };
     }
 
