c8dc0e413d58670b6b3ecc0640799b8cf28a5bc7
libing.chen <libing.chen@43b8a67c-e93d-0410-8a6d-b104505a36b8>
2008-12-09 08:15:40 +0000

    minor update

--- trunk/src/org/intellij/tiles2/filters/Struts2ReferenceDefinitionFilter.java
+++ trunk/src/org/intellij/tiles2/filters/Struts2ReferenceDefinitionFilter.java
@@ -1,29 +1,41 @@
 package org.intellij.tiles2.filters;
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.filters.ElementFilter;
+import com.intellij.patterns.ElementPattern;
+import com.intellij.patterns.ElementPatternCondition;
+import com.intellij.patterns.InitialPatternCondition;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.ProcessingContext;
 import org.intellij.tiles2.Tiles2Constants;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * struts2 result type of Tiles reference filter
  *
  * @author jacky
  */
-public class Struts2ReferenceDefinitionFilter implements ElementFilter {
+public class Struts2ReferenceDefinitionFilter implements ElementPattern<XmlTag> {
+    public boolean accepts(@Nullable Object o) {
+        return false;
+    }
 
-    public boolean isAcceptable(Object element, PsiElement context) {
-        if (context instanceof XmlTag) {
-            XmlTag tag = (XmlTag) context;
-            String resultType = tag.getAttributeValue("type");
-            if ("tiles".equals(resultType) && tag.getName().equals("result") && tag.getNamespace().equals(Tiles2Constants.STRUTS_CONFIG_DTD)) {
-                return true;
+    public boolean accepts(@Nullable Object o, ProcessingContext processingContext) {
+        return false;
+    }
+
+    public ElementPatternCondition<XmlTag> getCondition() {
+        return new ElementPatternCondition<XmlTag>(new InitialPatternCondition<XmlTag>(XmlTag.class) {
+            @Override
+            public boolean accepts(@Nullable Object o, ProcessingContext processingContext) {
+                if (o instanceof XmlTag) {
+                    XmlTag tag = (XmlTag) o;
+                    String resultType = tag.getAttributeValue("type");
+                    if ("tiles".equals(resultType) && tag.getName().equals("result") && tag.getNamespace().equals(Tiles2Constants.STRUTS_CONFIG_DTD)) {
+                        return true;
+                    }
+                }
+                return false;
             }
-        }
-        return false;
+        });
     }
 
-    public boolean isClassAcceptable(Class hintClass) {
-        return false;
-    }
 }
